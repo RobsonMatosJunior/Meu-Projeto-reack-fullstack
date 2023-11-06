332240078db0d4b593f0f4e9ac984ec20f0bb40c
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2023-11-06 16:22:44 -0300

    finish of aula 25

--- assets/2_FUNDAMENTOS/2_fundamentos/src/components/TemplateExpressions.jsx
+++ assets/2_FUNDAMENTOS/2_fundamentos/src/components/TemplateExpressions.jsx
@@ -1,13 +1,16 @@
 const TemplateExpressions = () => {
     const name = "Alguém";
-    const date ={
+    const data ={
         age: 31,
-        job:"programmer,"
+        job:"programmer",
     }
-    
+
     return(
         <div>
             <h1>Olá {name}, tudo bem?</h1>
+            <p>Você atua como: {data.job} </p>
+            <p>{4 + 4}</p>
+            <p>{console.log("JSX REACT")}</p>
         </div>
     );
 
