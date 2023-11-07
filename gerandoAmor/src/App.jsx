5238b6bfe20f5a5a54e80e0dfb3b7e0b5252b397
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2023-11-07 16:06:38 -0300

    add new section

---
+++ gerandoAmor/src/App.jsx
@@ -0,0 +1,21 @@
+// Imports
+import { useState } from 'react'
+import SectionInfos from './components/SectionInfos'
+
+// Import Styles
+import './App.css'
+import './SectionInfos.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <SectionInfos />
+
+     
+    </>
+  )
+}
+
+export default App
