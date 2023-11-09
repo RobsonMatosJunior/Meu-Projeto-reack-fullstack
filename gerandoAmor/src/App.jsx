fa7cae7ff9efdd2e4a439f26e278cd7afbd070d0
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2023-11-08 23:30:03 -0300

    new component

--- gerandoAmor/src/App.jsx
+++ gerandoAmor/src/App.jsx
@@ -1,19 +1,25 @@
 // Imports
 import { useState } from 'react'
 import SectionInfos from './components/SectionInfos'
+import SectionCarsd from './components/SectionCards'
 
 // Import Styles
 import './App.css'
 import './SectionInfos.css'
+import './SectionCards.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <SectionInfos />
-
-     
+      <section>
+        <SectionInfos />
+      </section>
+      <section>
+        <SectionCarsd />
+      </section>  
+      
     </>
   )
 }
---
+++ gerandoAmor/src/components/SectionCards.jsx
@@ -0,0 +1,65 @@
+
+const SectionCarsd =() => {
+
+    return(
+        <section className="sectionLayout">
+            
+            <div className="cards-layout">
+    
+                <div className="project-info">
+                    <img src="" alt="Icon handshake" />
+                    <h4>Seja um Voluntário</h4>
+                    <p>
+                        Toda ajuda é bem-vinda! Clique no botão abaixo e nos envie mensagem para entrarmos em contato!
+                    </p>
+                    <div className="btns-cards">
+                        <a className="know-more" href="" target="_blank">
+                            Voluntariado
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="cards-layout" >
+    
+                <div className="project-info">
+                    <img className="img2" src="" alt="Icon handshake" />
+                    <h4>Seja um Associado</h4>
+                    <p>
+                        Toda ajuda é bem-vinda! Clique no botão abaixo e nos envie mensagem para entrarmos em contato!
+                    </p>
+                    <div className="btns-cards">
+                        <a className="know-more" href="" target="_blank">
+                            Saiba Mais
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="card-dif">    
+                <div className="infos-card">
+                    <div className="card-style">
+                        <img className="layout" src="" alt="Icon @" />
+                        <h4>Nos Apoie!</h4>
+                        <p>
+                            Você pode nos ajudar de outra forma, compartilhe o nosso trabalho com outras pessoas, agradecemos!.
+                        </p>
+                        
+                        <p className="conteudo">
+                            Clique no Icone
+                            <img className="layout" src="" alt="seta"/>
+                            <img className="layout" src="" alt="Instagram"/>
+                        </p>
+                    </div>                    
+                </div>
+                <div>
+                    <img className="card-image" src=""/>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default SectionCarsd
